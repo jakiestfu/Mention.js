d2f700ddff1cb65d19efc9e4e6bf1dd66ed01006
Jacob Kelley <jakie8@gmail.com>
2012-12-13 12:26:59 -0800

    Created Mention.js

---
+++ mention.js
@@ -0,0 +1,72 @@
+var Mention = Mention || (function($) {
+
+    var opts = {
+        target: '',
+        users: [],
+        typeaheadOpts: {}
+    };
+
+    function _checkDependencies() {
+        if (typeof $ == 'undefined') {
+            throw new Error("jQuery is Required");
+        } else {
+            if (typeof $.fn.typeahead == 'undefined') {
+                throw new Error("Typeahead is Required");
+            }
+        }
+        return true;
+    }
+
+    function _matcher(item) {
+        item = item.toLowerCase();
+        var usernames = (this.query.toLowerCase()).match(/@\w+/g),
+            i;
+        if ( !! usernames) {
+            for (i = 0; i < usernames.length; i++) {
+                var username = (usernames[i].substring(1)).toLowerCase();
+                var matched = item.indexOf(username);
+
+                var re = new RegExp("@" + item, "g");
+                var used = ((this.query.toLowerCase()).match(re));
+
+                if (item.indexOf(username) != -1 && used === null) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    function _updater(item) {
+        var data = this.query;
+        var caratPos = this.$element[0].selectionStart;
+
+        for (i = caratPos; i >= 0; i--) {
+            if (data[i] == "@") {
+                break;
+            }
+        }
+
+        var replace = data.substring(i, caratPos);
+        data = data.replace(replace, '@' + item);
+
+        return data;
+    }
+
+    function applyMentions(userOpts) {
+
+        opts = $.extend(opts, userOpts);
+
+        if (_checkDependencies()) {
+            $(opts.target).typeahead($.extend({
+                source: opts.users,
+                matcher: _matcher,
+                updater: _updater
+            }, opts.typeaheadOpts));
+        }
+    }
+
+    return {
+        apply: applyMentions
+    };
+
+})(jQuery);
