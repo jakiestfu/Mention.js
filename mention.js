571d838e999ec4d7cf859d60d9359255be8af91b
Jacob Kelley <jakie8@gmail.com>
2012-12-17 15:54:08 -0800

    Updated Script
    Added jQuery plugin style-support, tightened up variable declarations, and added support for a custom delimiter instead of the @ symbol.

--- mention.js
+++ mention.js
@@ -1,80 +1,89 @@
 /*
- * MIT
- * Copyright (c) 2012 Jacob Kelley (jakiestfu.com, @jakiestfu)
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    Copyright (c) 2012 Jacob Kelley, @jakiestfu, http://jakiestfu.com
+	
+	Permission is hereby granted, free of charge, to any person obtaining
+	a copy of this software and associated documentation files (the
+	"Software"), to deal in the Software without restriction, including
+	without limitation the rights to use, copy, modify, merge, publish,
+	distribute, sublicense, and/or sell copies of the Software, and to
+	permit persons to whom the Software is furnished to do so, subject to
+	the following conditions:
+	
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+	LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+	OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+	WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
+(function($){
+    $.fn.extend({
+		mention: function(options) {
+			this.opts = {
+				users: [],
+                delimiter: '@',
+                typeaheadOpts: {}
+            };
 
-var Mention = Mention || (function($) {
+            var settings = $.extend({}, this.opts, options),
 
-    var opts = {
-        target: '',
-        users: [],
-        typeaheadOpts: {}
-    };
-
-    function _checkDependencies() {
-        if (typeof $ == 'undefined') {
-            throw new Error("jQuery is Required");
-        } else {
-            if (typeof $.fn.typeahead == 'undefined') {
-                throw new Error("Typeahead is Required");
-            }
-        }
-        return true;
-    }
-
-    function _matcher(item) {
-        item = item.toLowerCase();
-        var usernames = (this.query.toLowerCase()).match(/@\w+/g),
-            i;
-        if ( !! usernames) {
-            for (i = 0; i < usernames.length; i++) {
-                var username = (usernames[i].substring(1)).toLowerCase();
-                var matched = item.indexOf(username);
-
-                var re = new RegExp("@" + item, "g");
-                var used = ((this.query.toLowerCase()).match(re));
-
-                if (item.indexOf(username) != -1 && used === null) {
+                _checkDependencies = function() {
+                    if (typeof $ == 'undefined') {
+                        throw new Error("jQuery is Required");
+                    } else {
+                        if (typeof $.fn.typeahead == 'undefined') {
+                            throw new Error("Typeahead is Required");
+                        }
+                    }
                     return true;
+                },
+				_matcher = function(item) {
+					item = item.toLowerCase();
+                    var usernames = (this.query.toLowerCase()).match(new RegExp(settings.delimiter+'\\w+', "g")),
+                        i;
+                    if ( !! usernames) {
+                        for (i = 0; i < usernames.length; i++) {
+                            var username = (usernames[i].substring(1)).toLowerCase(),
+								matched = item.indexOf(username),
+								re = new RegExp(settings.delimiter + item, "g"),
+								used = ((this.query.toLowerCase()).match(re));
+			
+                            if (item.indexOf(username) != -1 && used === null) {
+                                return true;
+                            }
+                        }
+                    }
+                },
+				_updater = function(item) {
+                    var data = this.query,
+						caratPos = this.$element[0].selectionStart;
+			
+                    for (i = caratPos; i >= 0; i--) {
+                        if (data[i] == settings.delimiter) {
+                            break;
+                        }
+                    }
+			
+                    var replace = data.substring(i, caratPos);
+                    data = data.replace(replace, settings.delimiter + item);
+			
+                    return data;
+                };
+
+            return this.each(function() {
+                var _this = $(this);
+                if (_checkDependencies()) {
+                    _this.typeahead($.extend({
+                        source: settings.users,
+                        matcher: _matcher,
+                        updater: _updater
+                    }, settings.typeaheadOpts));
                 }
-            }
+            });
         }
-    }
-
-    function _updater(item) {
-        var data = this.query;
-        var caratPos = this.$element[0].selectionStart;
-
-        for (i = caratPos; i >= 0; i--) {
-            if (data[i] == "@") {
-                break;
-            }
-        }
-
-        var replace = data.substring(i, caratPos);
-        data = data.replace(replace, '@' + item);
-
-        return data;
-    }
-
-    function applyMentions(userOpts) {
-
-        opts = $.extend(opts, userOpts);
-
-        if (_checkDependencies()) {
-            $(opts.target).typeahead($.extend({
-                source: opts.users,
-                matcher: _matcher,
-                updater: _updater
-            }, opts.typeaheadOpts));
-        }
-    }
-
-    return {
-        apply: applyMentions
-    };
-
+    });
 })(jQuery);
